62d195a6e1495238504e68c02b73dcd5d2a44e0c
Ramonprojects <adm@ramonmago.com>
2026-05-12 19:19:13 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
   export default function handler(req, res) {                                                                                                                                              
-    res.writeHead(302, { Location: 'https://rboadodiab.sugestao.cc/' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=NF4J2KXLUFC&afp=bd&home=1' });
     res.end();                                                                                                                                                                             
   }
