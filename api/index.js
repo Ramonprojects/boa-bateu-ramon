4df982ccc4b6cc61c12cb539cc80b3d5a42b57dc
Ramonprojects <adm@ramonmago.com>
2026-05-12 18:16:48 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://rboadodiab.sugestao.cc/' });
+    res.end();                                                                                                                                                                             
+  }
