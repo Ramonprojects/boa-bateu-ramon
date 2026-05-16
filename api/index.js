c0256399b313342f48c901749a864e8bbdfb30a7
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-16 11:56:19 -0300

    chore: update shareCode to R0GH6Y99FG4

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=07ES0D7NZXR&afp=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=R0GH6Y99FG4&afp=bd&home=1' });
     res.end();
   }
