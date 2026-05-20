09f75f252a15548e19f2bdd1c847da8fbc9815b2
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-20 13:12:40 -0300

    chore: update shareCode to YSPRBO0A0MO

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=RTYMUQTKBFX&afp=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/iqnfrbwl?shareCode=YSPRBO0A0MO&afp=bd&home=1' });
     res.end();
   }
