93bf3df7ae7ca9def1765de169223655fc2ca38a
Ramonprojects <adm@ramonmago.com>
2026-06-07 09:57:29 -0300

    chore: shareCode A59409GHTHO -> 88O5NOS8J0R (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=A59409GHTHO&afp3=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=88O5NOS8J0R&afp3=bd&home=1' });
     res.end();
   }
