88286b11554de84c10e486938022afa298df4e39
Ramonprojects <adm@ramonmago.com>
2026-06-08 18:47:19 -0300

    chore: shareCode N9H5LA3WVLP -> I0XF7UHDCKU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=N9H5LA3WVLP&afp3=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=I0XF7UHDCKU&afp3=bd&home=1' });
     res.end();
   }
