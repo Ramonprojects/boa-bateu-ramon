fe0a66c1d79b3d3661079c30ccf708b4043750ff
Ramonprojects <adm@ramonmago.com>
2026-06-09 10:31:39 -0300

    chore: shareCode I0XF7UHDCKU -> 5QOUKVAF0DY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=I0XF7UHDCKU&afp3=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vqjid4fa?shareCode=5QOUKVAF0DY&afp3=bd&home=1' });
     res.end();
   }
