52d944f3831e27f768cd8861ad5696d83edf6393
Ramonprojects <adm@ramonmago.com>
2026-06-22 12:48:08 -0300

    chore: shareCode DS11U2IRUEL -> GLE2EN1NPUQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=DS11U2IRUEL&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=GLE2EN1NPUQ&afp2=bd&home=1' });
     res.end();
   }
