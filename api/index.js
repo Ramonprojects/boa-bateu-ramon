b7244da0d99ac28c5c693115c209517bacc6b20d
Ramonprojects <adm@ramonmago.com>
2026-06-23 11:31:51 -0300

    chore: shareCode GLE2EN1NPUQ -> UMJ4CCYFT2N (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=GLE2EN1NPUQ&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=UMJ4CCYFT2N&afp2=bd&home=1' });
     res.end();
   }
