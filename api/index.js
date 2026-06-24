446dfb857308072e1ae11d12c51ae79dc68efb74
Ramonprojects <adm@ramonmago.com>
2026-06-24 13:15:44 -0300

    chore: shareCode UMJ4CCYFT2N -> QGYRNB8ZN90 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=UMJ4CCYFT2N&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=QGYRNB8ZN90&afp2=bd&home=1' });
     res.end();
   }
