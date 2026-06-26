5f88f987463aa87063d5c01431f1f9ab2de2c872
Ramonprojects <adm@ramonmago.com>
2026-06-26 18:25:25 -0300

    chore: shareCode WK0IFJXETEG -> EWGOBR9VONB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=WK0IFJXETEG&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=EWGOBR9VONB&afp2=bd&home=1' });
     res.end();
   }
