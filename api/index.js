0ca1103ba8a93182476a5f22f8e4c8339d27f6f0
Ramonprojects <adm@ramonmago.com>
2026-07-01 11:46:36 -0300

    chore: shareCode 8UDBJUATQFA -> 1ONYPLO3R77 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=8UDBJUATQFA&afp2=bd&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/ihryf4s5?campaign_id=29516&shareCode=1ONYPLO3R77&afp2=bd&home=1' });
     res.end();
   }
